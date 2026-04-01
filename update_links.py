1e44e4323397dd7c2206c39dd4c61f09696660fe
ray059 <50396895+ray059@users.noreply.github.com>
2026-03-31 21:25:14 -0500

    Update source name for science and technology links

--- update_links.py
+++ update_links.py
@@ -42,7 +42,7 @@ RSS_SOURCES = {
 
     # 🔹 NUEVO: CIENCIA Y TECNOLOGIA
     "ciencia y tecnología": {
-        "DW.COM": "https://rss.dw.com/rdf/rss-sp-cyt",
+        "DW Español": "https://rss.dw.com/rdf/rss-sp-cyt",
         "El Tiempo": "https://www.eltiempo.com/rss/vida_ciencia.xml",
         "El Tiempo": "https://www.eltiempo.com/rss/tecnosfera.xml"
     }
